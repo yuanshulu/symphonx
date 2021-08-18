93791f20da2dc624f1d3cff532013e39c1bf851a
呂沅澍 <zero5405@gmail.com>
2021-08-18 14:55:57 +0800

    update mockData

--- src/assets/js/mockData.js
+++ src/assets/js/mockData.js
@@ -8,59 +8,41 @@ export default (path) => {
       resp.data = {
         orders: [
           {
-            name: "贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
-            status: {
-              code: 1,
-              type: "處理中",
-            },
-            date: "102/6/2",
-          },
-          {
-            name: "Livi優活 抽取式衛生紙(100抽x10包x10串/箱)",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+            name: 'Livi優活 抽取式衛生紙(100抽x10包x10串/箱)',
+            logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
             status: {
               code: 3,
-              type: "已取消",
+              type: '已取消'
             },
-            date: "101/6/12",
-          },
-          {
-            name: "Livi優活 抽取式衛生紙(100抽x10包x10串/箱)",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
-            status: {
-              code: 3,
-              type: "已取消",
-            },
-            date: "107/6/12",
-          },
-          {
-            name: "BALMUDA The Toaster 百慕達烤麵包機-黑色",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+            date: '107/6/12'
+        },
+        {
+            name: 'BALMUDA The Toaster 百慕達烤麵包機-黑色',
+            logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
             status: {
               code: 2,
-              type: "已成立",
+              type: '已成立'
             },
-            date: "108/7/21",
-          },
-          {
-            name: "贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+            date: '108/7/21'
+        },
+        {
+            name: '贈-短慧萬用鍋HD2133+三合一濾網「LG樂金」韓國原裝...',
+            logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
             status: {
               code: 1,
-              type: "處理中",
+              type: '處理中'
             },
-            date: "108/6/2",
-          },
-          {
-            name: "Apple AirPds 2",
-            logo: "https://static.oopocket.com/store/iconTreemall@3x.png",
+            date: '108/6/2'
+         },
+         {
+            name: 'Apple AirPds 2',
+            logo: 'https://static.oopocket.com/store/iconTreemall@3x.png',
             status: {
               code: 4,
-              type: "已送達",
+              type: '已送達'
             },
-            date: "108/3/02",
-          },
+            date: '108/3/02'
+        }
         ],
       };
       break;
